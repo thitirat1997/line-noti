4fc89aa680936589edbb41eeb05177b41cb7a783
thitirat <thitirat.hirun1997@gmail.com>
2021-07-27 10:35:11 +0700

    update

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+// import liff from '@line/liff';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'line-noti';
+  
+}
+// liff.init({ liffId: 'myLiffId' });
